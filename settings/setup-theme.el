f087e18cee7bca4d45932d76b129a5742b62c9d3
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-04-27 00:25:23 +0200

    theme: monokai -> jbeans

--- settings/setup-theme.el
+++ settings/setup-theme.el
@@ -1,8 +1,10 @@
 (require 'use-package)
-(use-package monokai-theme
+(use-package jbeans-theme
   :defer t
   :ensure t
   :init
-  (load-theme 'monokai t)
-  (global-hl-line-mode))
+  (load-theme 'jbeans t)
+  (global-hl-line-mode)
+  (add-to-list 'default-frame-alist '(font . "mononoki-12")))
+
 (provide 'setup-theme)
