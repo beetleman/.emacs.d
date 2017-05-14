cdb02a31f1235c09b915f3a7894ad77d29a94bb9
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-05-14 23:18:33 +0200

    add tern to js config

--- settings/setup-js.el
+++ settings/setup-js.el
@@ -1,16 +1,29 @@
 (require 'use-package)
+
 (use-package js2-mode
   :ensure t
   :init
+  (use-package rjsx-mode
+    :ensure t
+    :init
+    (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+    (add-to-list 'auto-mode-alist '("\\.react.js\\'" . rjsx-mode))
+    (add-to-list 'auto-mode-alist '("\\index.android.js\\'" . rjsx-mode))
+    (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . rjsx-mode))
+    (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . rjsx-mode))
+    (add-to-list 'magic-mode-alist '("^import React" . rjsx-mode)))
+
+  (use-package tern
+    :diminish " T"
+    :commands (tern-mode)
+    :init
+    (add-hook 'js-mode-hook 'tern-mode))
+
+  (use-package company-tern
+    :config
+    (add-to-list 'company-backends 'company-tern))
+
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
 
-(use-package rjsx-mode
-  :ensure t
-  :init
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("\\.react.js\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("\\index.android.js\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . rjsx-mode))
-  (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . rjsx-mode))
-  (add-to-list 'magic-mode-alist '("^import React" . rjsx-mode)))
 (provide 'setup-js)
