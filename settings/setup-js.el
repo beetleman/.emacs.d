17f7cee9033898367134ee24ad045245d414ff08
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-02-01 13:29:42 +0100

    use `js2-xref` instead `tern-mode` from finding definitions

--- settings/setup-js.el
+++ settings/setup-js.el
@@ -24,8 +24,19 @@
     :ensure t
     :diminish "T"
     :commands (tern-mode)
+    :config
+    (define-key tern-mode-keymap (kbd "M-.") nil)
+    (define-key tern-mode-keymap (kbd "M-,") nil)
     :init
     (add-hook 'js-mode-hook 'tern-mode))
+
+  (use-package xref-js2
+    :ensure t
+    :config
+    (define-key js-mode-map (kbd "M-.") nil)
+    (add-hook 'js2-mode-hook (lambda ()
+                               (add-hook 'xref-backend-functions
+                                         #'xref-js2-xref-backend nil t))))
 
   (use-package company-tern
     :ensure t
