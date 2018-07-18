e12210cf10f6245cecfae1798ec9c0329b546def
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-07-19 01:49:23 +0200

    fix problem with emoji in deamon mode

--- settings/setup-theme.el
+++ settings/setup-theme.el
@@ -1,4 +1,8 @@
 (require 'use-package)
+
+(defun setup-emoji (frame)
+  (set-fontset-font t 'symbol "Noto Emoji" frame 'prepend))
+
 (use-package tao-theme
   :defer t
   :ensure t
@@ -7,9 +11,10 @@
   (load-theme 'tao-yin t)
   (global-hl-line-mode 1)
   (column-number-mode 1)
+  (create-fontset-from-fontset-spec standard-fontset-spec)
   (add-to-list 'default-frame-alist '(font . "Fantasque Sans Mono-13"))
   (add-to-list 'default-frame-alist '(cursor-color . "white"))
-  (when (member "Noto Emoji" (font-family-list))
-    (set-fontset-font t 'unicode "Noto Emoji" nil 'prepend)))
+  (add-hook 'after-make-frame-functions 'setup-emoji)
+  (setup-emoji nil))
 
 (provide 'setup-theme)
