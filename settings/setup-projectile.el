b254281869f229f636beac8ca21bb8442e73832c
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-08-19 19:40:34 +0200

    update config frot projectile

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -2,9 +2,13 @@
 (use-package projectile
   :ensure t
   :init
-  (projectile-mode 1))
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
 (use-package counsel-projectile
   :ensure t
   :config
   (counsel-projectile-mode 1))
+
 (provide 'setup-projectile)
