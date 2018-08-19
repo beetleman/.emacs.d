0c69f5f45c60dd19dcacc5967cc1d71bba24f98d
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-08-19 19:36:16 +0200

    use multimarkdown instead of python-markdown

--- settings/setup-markdown.el
+++ settings/setup-markdown.el
@@ -7,7 +7,7 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init
-  (setq markdown-command "/usr/bin/markdown_py-3 -x gfm")
+  (setq markdown-command "multimarkdown")
   (let ((gfm-css (file-truename "~/.emacs.d/settings/setup-markdown.css")))
     (when (file-exists-p gfm-css)
       (setq markdown-css-paths `(,gfm-css)))))
