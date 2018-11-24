bd8da20a6ff19f16fdad02629cab0c2e73a178dd
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-11-24 19:43:16 +0100

    nyan-mode

--- settings/setup-mode-line.el
+++ settings/setup-mode-line.el
@@ -3,6 +3,10 @@
   :ensure t
   :config
   (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'respectful)
-  (sml/setup))
+  (setq sml/theme 'dark)
+  (sml/setup)
+  (use-package nyan-mode
+    :ensure t
+    :config
+    (nyan-mode +1)))
 (provide 'setup-mode-line)
