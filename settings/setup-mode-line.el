e1427aa6e9098f970e1ca2ba3467c3f6291aedaa
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-11-30 23:01:58 +0100

    remove nyan-mode;(

--- settings/setup-mode-line.el
+++ settings/setup-mode-line.el
@@ -4,9 +4,5 @@
   :config
   (setq sml/no-confirm-load-theme t)
   (setq sml/theme 'dark)
-  (sml/setup)
-  (use-package nyan-mode
-    :ensure t
-    :config
-    (nyan-mode +1)))
+  (sml/setup))
 (provide 'setup-mode-line)
