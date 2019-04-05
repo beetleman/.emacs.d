51a7a46736975030de1cb8cbcfa2af7762626163
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2019-04-05 12:28:11 +0200

    simplify clojure setup

--- settings/setup-clojure.el
+++ settings/setup-clojure.el
@@ -12,19 +12,7 @@
   (add-hook 'cider-mode-hook #'clj-refactor-mode)
   (add-hook 'cider-mode-hook #'smartparens-mode)
   (add-hook 'cider-mode-hook #'eldoc-mode)
-  :diminish subword-mode
-  :config
-  (setq nrepl-log-messages t
-        cider-repl-display-in-current-window t
-        cider-repl-use-clojure-font-lock t
-        cider-prompt-save-file-on-load 'always-save
-        cider-font-lock-dynamically '(macro core function var)
-        nrepl-hide-special-buffers t
-        cider-overlays-use-font-lock t
-        cider-cljs-lein-repl "(do (require 'figwheel-sidecar.repl-api)
-                                  (figwheel-sidecar.repl-api/start-figwheel!)
-                                  (figwheel-sidecar.repl-api/cljs-repl))")
-  (cider-repl-toggle-pretty-printing))
+  :diminish subword-mode)
 
 
 (use-package cider-eval-sexp-fu
@@ -34,8 +22,13 @@
 (use-package clj-refactor
   :defer t
   :ensure t
-  :diminish clj-refactor-mode
-  :config (cljr-add-keybindings-with-prefix "C-c C-m"))
+  :diminish clj-refactor-mode)
 
+(use-package flycheck-joker
+  :ensure t
+  :init
+  (require 'flycheck-joker)
+  (defun clj-joker-hook () (flycheck-mode 1))
+  (add-hook 'clojure-mode-hook #'clj-joker-hook))
 
 (provide 'setup-clojure)
