41c2723ff0642b33c5fe521b6f90eb9f3d0a60cc
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2019-04-05 12:27:58 +0200

    flymake for lsp

--- settings/setup-lsp.el
+++ settings/setup-lsp.el
@@ -4,9 +4,17 @@
   :ensure t
   :config
   (require 'lsp-clients)
+  (setq lsp-prefer-flymake nil
+        lsp-ui-sideline-enable nil
+        lsp-ui-imenu-enable nil
+        lsp-ui-doc-enable nil
+        lsp-ui-peek-enable nil)
+
+  (use-package lsp-ui
+    :ensure t)
+
   (use-package company-lsp
     :ensure t
     :commands company-lsp))
-
 
 (provide 'setup-lsp)
