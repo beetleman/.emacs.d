84a19730d82c9bcf612e64e6d4ccdc08ae460685
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2019-08-29 21:59:37 +0200

    use lsp-rust instead racer

--- settings/setup-rust.el
+++ settings/setup-rust.el
@@ -4,18 +4,12 @@
   :ensure t
   :defer t
   :config
-  (use-package company-racer
-    :ensure t)
   (use-package cargo
     :ensure t)
   (use-package flycheck-rust
     :ensure t
     :config
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-  (use-package racer
-    :ensure t
-    :config
-    (add-hook 'rust-mode-hook #'racer-mode)
-    (add-hook 'racer-mode-hook #'eldoc-mode)))
+  (add-hook 'rust-mode-hook #'lsp))
 
 (provide 'setup-rust)
