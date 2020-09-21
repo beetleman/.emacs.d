9e77ece18e9531f74a804b59972a7d86345055c0
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2020-09-21 19:17:28 +0200

    improve compoany setup

--- settings/setup-company.el
+++ settings/setup-company.el
@@ -1,13 +1,23 @@
 (use-package company
-  :bind
-  ("M-i" . company-complete)
-  :config
+  :bind (("M-i" . company-complete)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :init
+  (setq company-idle-delay 0.5)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
   (setq company-tooltip-align-annotations t)
-  (setq company-minimum-prefix-length 1)
-  :init (global-company-mode))
+  ;; invert the navigation direction if the the completion popup-isearch-match
+  ;; is displayed on top (happens near the bottom of windows)
+  (setq company-tooltip-flip-when-above t)
+  :config (global-company-mode t))
 
 (use-package company-quickhelp
   :init
+  (setq company-quickhelp-delay nil)
+  :config
   (company-quickhelp-mode))
 
 (provide 'setup-company)
