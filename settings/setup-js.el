0e621cbcd3d79b9f92ed9fcb5eeb09b8c15ea886
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2020-10-29 17:16:31 +0100

    update js config

--- settings/setup-js.el
+++ settings/setup-js.el
@@ -1,19 +1,9 @@
 (require 'use-package)
 
-(use-package add-node-modules-path
-  :hook (js-mode . add-node-modules-path))
-
-(use-package prettier-js
-  :hook (js-mode . prettier-js-mode))
-
-(use-package vue-mode
-  :mode "\\.vue\\'"
-  :init
-  (setq mmm-submode-decoration-level 0))
-
-
 (use-package web-mode
-  :mode "\\.jsx\\'")
-
+  :mode ("\\.js[x]?\\'" "\\.vue\\'")
+  :config
+  (setq web-mode-content-types-alist
+  '(("jsx"  . "\\.js[x]?\\'"))))
 
 (provide 'setup-js)
