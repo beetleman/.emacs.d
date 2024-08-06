ffc39dae1d2dc297e2c0286ae94dd86a46d0cc03
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2024-08-06 11:20:31 +0200

    disable transparent titlebar

--- early-init.el
+++ early-init.el
@@ -13,6 +13,4 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
 (setq-default mode-line-format nil)
