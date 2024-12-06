15f5f0f54f1d01ad4a12069449f3f3e117ae26f8
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2024-12-06 12:16:35 +0100

    cleanup frame-title

--- early-init.el
+++ early-init.el
@@ -13,4 +13,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+
 (setq-default mode-line-format nil)
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
