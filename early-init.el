6db3e3d991534aa1c144178f8366c46cb67c43c7
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2024-12-16 10:17:34 +0100

    improve frame config

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 ;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
 
 (setq gc-cons-threshold most-positive-fixnum)
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
 
 ;; I handle package init in `init.el' file
 (setq package-enable-at-startup nil)
@@ -10,9 +13,17 @@
 (setq frame-inhibit-implied-resize t)
 
 ;; Disable not used visual elements
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+(setq default-frame-alist '(;; You can turn off scroll bars by uncommenting these lines:
+                            (vertical-scroll-bars . nil)
+                            (horizontal-scroll-bars . nil)
+                            (menu-bar-lines . 0)
+                            (tool-bar-lines . 0)
+                            ;; Setting the face in here prevents flashes of
+                            ;; color as the theme gets activated
+                            (background-color . "#000000")
+                            (foreground-color . "#ffffff")
+                            (ns-appearance . dark)
+                            (ns-transparent-titlebar . t)))
 
 (setq-default mode-line-format nil)
 (setq ns-use-proxy-icon nil)
