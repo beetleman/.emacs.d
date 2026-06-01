20be8caa6d5e045abf2253031369ab6b4524fdbf
Mateusz Probachta-Jeżowski <mateusz.probachta@vertexinc.com>
2026-06-01 13:20:56 +0200

    update xml lsp to 0.31.1

--- bin/download_external_tools.sh
+++ bin/download_external_tools.sh
@@ -3,4 +3,4 @@
 echo download xml lsp
 LEMMINX_DIR=~/.emacs.d/share/lemminx
 mkdir -p $LEMMINX_DIR
-curl https://download.eclipse.org/lemminx/releases/0.27.1/org.eclipse.lemminx-uber.jar -o $LEMMINX_DIR/org.eclipse.lemminx-uber.jar
+curl https://download.eclipse.org/lemminx/releases/0.31.1/org.eclipse.lemminx-uber.jar -o $LEMMINX_DIR/org.eclipse.lemminx-uber.jar
